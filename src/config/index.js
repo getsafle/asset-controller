8d2a188a0a7d93896f429c3971a8470ad0ab075e
shubham <shubham@getsafle.com>
2022-01-10 15:35:05 +0530

    implemented dynamic selection of chains

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,14 @@
 module.exports = {
-    ETH_SINGLE_CALL_BALANCES_ADDRESS: '0xb1f8e55c7f64d203c1400b9d8555d050f94adf39',
-    ETH_CONTRACT_MAP_URL: 'https://gist.githubusercontent.com/getsafle-org/3f50226d1d1c03803f2283a090f6f745/raw/f46efa8b5edd957f778470bcc96a7b7be62917b2/eth_contract_map.json',
-    POLYGON_SINGLE_CALL_BALANCES_ADDRESS: '0x38a2718e3dff5faee56416ba0b14aae8eb803c22',
-    POLYGON_CONTRACT_MAP_URL: 'https://gist.githubusercontent.com/getsafle-org/3bfdace61ee636728a5d55ea1c21cf08/raw/f6587692b56ea2edab11159001f81607dd34335a/polygon_contract_map.json',
-}
+    chains: {
+      "ethereum": {
+        SINGLE_CALL_BALANCES_ADDRESS: '0xb1f8e55c7f64d203c1400b9d8555d050f94adf39',
+        CONTRACT_MAP_URL: 'https://gist.githubusercontent.com/getsafle-org/3f50226d1d1c03803f2283a090f6f745/raw/f46efa8b5edd957f778470bcc96a7b7be62917b2/eth_contract_map.json',
+      },
+      "polygon": {
+        SINGLE_CALL_BALANCES_ADDRESS: '0x38a2718e3dff5faee56416ba0b14aae8eb803c22',
+        CONTRACT_MAP_URL: 'https://gist.githubusercontent.com/getsafle-org/3bfdace61ee636728a5d55ea1c21cf08/raw/f6587692b56ea2edab11159001f81607dd34335a/polygon_contract_map.json',
+      }
+    },
+    supportedChains: ['ethereum', 'polygon']
+  };
+  
--- src/utils/helper.js
+++ src/utils/helper.js
@@ -4,23 +4,15 @@ const axios = require('axios');
 const { INVALID_CHAIN_SELECTED } = require('../constants/responses');
 
 const getChainDetails = async (chain) => {
-    let SINGLE_CALL_BALANCES_ADDRESS;
-
-    switch (chain) {
-        case 'ethereum': {
-            const { response } = await getRequest({ url: config.ETH_CONTRACT_MAP_URL });
-            SINGLE_CALL_BALANCES_ADDRESS = config.ETH_SINGLE_CALL_BALANCES_ADDRESS;
-            return { contracts: response, SINGLE_CALL_BALANCES_ADDRESS };
-        }
-        case 'polygon':{
-        const { response } = await getRequest({ url: config.POLYGON_CONTRACT_MAP_URL });
-        SINGLE_CALL_BALANCES_ADDRESS = config.POLYGON_SINGLE_CALL_BALANCES_ADDRESS;
-        return { contracts: response, SINGLE_CALL_BALANCES_ADDRESS };
-        }
-        default: {
-            return { error: INVALID_CHAIN_SELECTED };
-        }
+    
+    if(!config.supportedChains.includes(chain))
+    {
+        return { error: INVALID_CHAIN_SELECTED };
     }
+   else{
+    const { response } = await getRequest({ url: config.chains[chain].CONTRACT_MAP_URL });
+    return { contracts: response, SINGLE_CALL_BALANCES_ADDRESS: config.chains[chain].SINGLE_CALL_BALANCES_ADDRESS };
+   }
 }
 
 const getRequest = async ({ url }) => {
